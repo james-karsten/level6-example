532a2251f3f42aadf43083c5ebc7953812e46f9e
karstej <jameskarsten007@gmail.com>
2020-05-23 18:26:41 +0200

    level6example

---
+++ app/src/main/java/com/example/level6example/NumbersRepository.kt
@@ -0,0 +1,8 @@
+package com.example.level6example
+
+class NumbersRepository {
+
+    private val numbersApi: NumbersApiService = NumbersApi.createApi()
+
+    fun getRandomNumberTrivia() = numbersApi.getRandomNumberTrivia()
+}
